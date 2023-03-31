7d22229e1de19c792256d8447da5c96da801c238
Paulo Vítor <paulovmedeiros@outlook.com>
2023-03-31 10:25:07 -0300

    api: creating WorkViewSet

--- api/TodoApp/todo/views.py
+++ api/TodoApp/todo/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import WorkTodo
+from .serializers import WorkSerializer
+
+from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+class WorkViewSet(viewsets.ModelViewSet):
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = WorkTodo.objects.all()
+    serializer_class = WorkSerializer
