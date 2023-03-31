854cb1b9a4591bc642454614b2b2d53913de6588
Paulo Vítor <paulovmedeiros@outlook.com>
2023-03-31 10:20:17 -0300

    creating WorkTodo model

---
+++ api/TodoApp/todo/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class WorkTodo(models.Model):
+    WORKTODO = 'worktodo'
+    WORKDONE = 'workdone'
+
+    STATUS_CHOICES = (
+        (WORKTODO, 'Work To Do')
+        (WORKDONE, 'Work Done')
+    )
+
+    work_description = models.CharField(max_length=255)
+    work_status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=WORKTODO)
+    
