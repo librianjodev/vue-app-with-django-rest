71d25286ccfb65f865dde3547df3a1278906d545
Paulo Vítor <paulovmedeiros@outlook.com>
2023-03-31 10:38:43 -0300

    api: creating router

--- api/TodoApp/TodoApp/urls.py
+++ api/TodoApp/TodoApp/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+
+from   todo.views import WorkViewSet
+
+router = routers.DefaultRouter()
+router.register(r'tasks', WorkViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
